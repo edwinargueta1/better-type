67e752bd961ed943abd0752472ce0f92a90ff25b
Edwin Argueta <46907039+edwinargueta1@users.noreply.github.com>
2024-03-20 21:57:15 -0700

    Random Word Generator
    Developed a functional component to a pick 10 random words from a file to display on the browser.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import './dictionary.txt';
+// import './dictionary.txt';
 import React, {useState, useEffect} from 'react';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
 
       <div id='background'>
         <div id='textBox'>
-          <p id='text' onLoad={change()}>words</p>
+          <Change/>
         </div>
       </div>
       
@@ -17,47 +17,39 @@ function App() {
   );
 }
 
-function change(){
-  // let text = document.getElementById(text);
-  // console.log(text);
-}
+ function Change(){
+  const [randomWords, setRandomWords] = useState([]);
 
-function randomWords() {
-  let listOfWords = [];
+  let generateRandomWords = async() => {
 
-  fetch('./dictionary.txt')
-  .then(response => response.text())
-  .then(data => {
-    console.log(data); 
-    listOfWords = data;// Do something with the file content
-  })
-  .catch(error => {
-    console.error('Error reading file:', error);
-  });
-  
-  // let fs = require('fs');
-  // fs.readFile('./dictionary.txt' , function(err, data){
-  //   if(err) throw err;
-  //   var array = data.toString().split('\n');
-  //   for(let i in array){
-  //     console.log(array[i])
-  //   }
-  // })
-  // let file = require('./dictionary.txt');
-
-  // // console.log(file);
-  // fetch('./dictionary.txt')
-  // .then(response => response.text())
-  // .then(TEXT => {
-  //   dict = TEXT.toString().split('\n');
-  // })
-  // dict = [1,2,3,4,  'bruh']
-  // console.log(dict);
-  // file.toString().split('\n');
-  for (let i = 0; i < 10; i++) {
-
+    let text = await fetch('/dictionary.txt')
+      .then((res) => { return res.text() })
+      .then((stuff) => {
+         return stuff.split('\r\n');
+      }).catch(err => {return `File  Not Found. ${err}`})
+    
+    //All the values are put into an array
+    //Then we need to randomise what gets used
+    let randomIndex = new Array(10).fill().map(() => Math.floor(Math.random() * 10000));
+    // randomIndex.forEach((e, i) => {randomIndex[i] = Math.floor(Math.random()*10000)});
+    let words = [];
+    randomIndex.forEach((e, i) => {words.push(text[randomIndex[i]])})
+    setRandomWords(words)
   }
 
+  return(
+    <>
+    <div>
+      <button onClick={generateRandomWords}>Generate Random Words</button>
+      <p>Random Words:</p>
+      <ul>
+        {randomWords.map((word, index) => (
+          <p key={index}>{word}</p>
+        ))}
+      </ul>
+    </div>
+    </>
+  )
 }
 
 export default App;
