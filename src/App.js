5fbc48b46d0e0c103e34ac476eb48262a6863b00
Edwin Argueta <46907039+edwinargueta1@users.noreply.github.com>
2024-04-01 22:10:52 -0700

    Core Functionality
    Created a way to listen for keyboard input
    Verified if the input was valid for the character in the phrase
    Provided feedback of completion in phrase
    Created Error Counter
    Created Accuracy Tracker
    Enlarged the font

--- src/App.js
+++ src/App.js
@@ -17,35 +17,32 @@ function TextBox() {
   const [dictionary, setDictionary] = useState([]);
   const [randomWords, setRandomWords] = useState([]);
   const [keyboard, setKeyboard] = useState([]);
-  // const [error, setError] = useState([]);
 
+  const [error, setError] = useState(0);
+  const [accuracy, setAccuracy] = useState(100);
+  let repeat = false;
 
-  async function buildDictionary() {
-    //Text Stores all the words in dictionary into an array
-    let text = await fetch('/dictionary.txt')
-      .then((res) => { return res.text() })
-      .then((stuff) => {
-        return stuff.split('\r\n');
-      }).catch(err => { return `File  Not Found. ${err}` })
-    setDictionary(text);
-  }
 
   useEffect(() => {
-     // Call getRandomWords after setting dictionary
-    getRandomWords();
-    console.log('callled getRandomWords')
-    console.log(randomWords);
-  },[dictionary]); // Call getRandomWords whenever dictionary changes
-
+    async function buildDictionary() {
+      //Text Stores all the words in dictionary into an array
+      let text = await fetch('/dictionary.txt')
+        .then((res) => { return res.text() })
+        .then((stuff) => {
+          return stuff.split('\r\n');
+        }).catch(err => { return `File  Not Found. ${err}` })
+      setDictionary(text);
+    }
+    buildDictionary();
+  }, []); // Call getRandomWords whenever dictionary changes
 
   //Stores letters of random words in to an array
   function getRandomWords() {
-    // console.log(dictionary[0]);
     let newRandomWords = [];
+    console.log(`Dictionary: ${dictionary[0]}`);
     for (let i = 0; i < 10; i++) {
       let randomIndex = Math.floor(Math.random() * 10000);
-      // let word = dictionary[randomIndex];
-      let word = 'hellobruh';
+      let word = dictionary[randomIndex];
       for (let j = 0; j < word.length; j++) {
         newRandomWords.push(word.charAt(j));
       }
@@ -53,50 +50,53 @@ function TextBox() {
     }
     setRandomWords(newRandomWords);
   }
-  useEffect(() => {
-    console.log(`Updated dictionary! ${dictionary.length}`)
-  },[dictionary])
+  
+
 
   useEffect(() => {
-    console.log(`Updated randomWords! ${randomWords}`)
+    if(dictionary.length > 0){
+      // Call getRandomWords after setting dictionary
+      getRandomWords();
+    }
+  },[dictionary])
+
+
+  //DEBUGGING
+  useEffect(() => {
+    // Add event listener when the component mounts
+    document.addEventListener('keydown', handleKeyPress);
+    // Remove event listener when the component unmounts
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+
   },[randomWords])
 
   //So as this component load it will Load all the available words
   let onlyLetters = new RegExp("[A-Za-z\\s]");
 
-
-
-  useEffect(() => {
-    console.log('dict')
-    buildDictionary();
-    // Event listener to handle keydown events
-    const handleKeyDown = (event,dictionary) => {
-      handleKeyPress(event, dictionary);
-    };
-
-    // Add event listener when the component mounts
-    document.addEventListener('keydown', handleKeyDown);
-
-    // Remove event listener when the component unmounts
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
-
-
-
-  function handleKeyPress(event, dictionary) {
+  function handleKeyPress(event) {
     //if the letter is valid
-    console.log(dictionary)
-    console.log(dictionary[0])
     if (event.key.length === 1 && onlyLetters.test(event.key) && randomWords[0] === event.key) {
-      const newRandomWords = newRandomWords.slice(0);
+      let newRandomWords = randomWords.slice(1);
       setKeyboard(prevInput => [...prevInput, event.key]);
       setRandomWords(newRandomWords);
+      repeat = false;
+
+      //If there is no more letters then restart
+      if (randomWords.length === 1) {
+        getRandomWords();
+        setKeyboard([]);
+        setError(0);
+      }
+    }else{
+      if(repeat === false){
+        setError((prevError) => prevError = prevError + 1);
+        repeat = true;
+      }
     }
 
-
-    //We need to take input form out of the box of the input 
+    //We need to take input form out of the box of the input
 
     //We take in the event add it to the new tag only if;
     //it is a part of the first letter in the array of randomWords
@@ -105,13 +105,23 @@ function TextBox() {
 
     //The error will be the current wrong letter turned RED.
   }
+  //Will recalculate with every key stroke
+  useEffect(() => {
+    calculateAccuracy();
+  }, [keyboard]) 
+  //
+  function calculateAccuracy(){
+    let newAccuracy = Number.isNaN((keyboard.length - error) / keyboard.length) ? 0 : (((keyboard.length - error) / keyboard.length)*100).toFixed(2);
+    let formated = (newAccuracy% 1 === 0 ) ? Math.floor(newAccuracy) : (newAccuracy);
+    setAccuracy(formated);
+  }
 
-  console.log(dictionary)
   return (
     <>
       <div>
-        <p>Dictionary Length: {dictionary.length}</p>
         <p id='textToBeCompleted'><span id='completedLetters'>{keyboard}</span>{randomWords}</p>
+        <p id='errorCount'>Errors: {error}</p>
+        <p>Accuracy: {accuracy}%</p>
       </div>
     </>
   )
