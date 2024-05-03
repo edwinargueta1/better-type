0de40dd810e04c81de32f5a144aed3c8635c3fa0
Edwin Argueta <46907039+edwinargueta1@users.noreply.github.com>
2024-05-03 03:48:34 -0700

    added database upload and database fix
    installed database emulator
    added create new user function
    uploaded a user with basic information
    added username length restriction
    added no duplicate username restriction
    created environment variables for api keys
    created new api keys

--- src/Components/SignUpPopUp.jsx
+++ src/Components/SignUpPopUp.jsx
@@ -1,42 +1,71 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
+import { auth, createNewUser, database } from "../config/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, doc, getDoc, query, where } from "firebase/firestore";
 
 export default function SignUpPopUp({ popUpState, setPopUp, toggleState }) {
-
   const validEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const [signUpError, setSignUpError] = useState("");
-  const [newUser, setNewUser] = useState({ username : "", email: "", password: "", reEnterPassword: ""});
+  const [signUpError, setSignUpIndicator] = useState("");
+  const [newUser, setNewUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    reEnterPassword: "",
+  });
 
-  function validSignUp(){
+  async function validSignUp() {
+    //username too long
+    if(newUser.username.length > 16){
+      setSignUpIndicator("Username must be 16 characters or less.")
+      return false;
+    }
     //Valid password
-    if(newUser.password.length < 8){
-      setSignUpError("Password must be 8 characters or longer.")
+    if (newUser.password.length < 6) {
+      setSignUpIndicator("Password must be 6 characters or longer.");
       return false;
     }
     //If both passwords don't match
-    if(newUser.password !== newUser.reEnterPassword){
-      setSignUpError("Passwords do not match.")
+    if (newUser.password !== newUser.reEnterPassword) {
+      setSignUpIndicator("Passwords do not match.");
       return false;
     }
     //email needs too look like an email
-    if(!validEmailRegex.test(newUser.email)){
-      setSignUpError("Not a valid email format.")
+    if (!validEmailRegex.test(newUser.email)) {
+      setSignUpIndicator("Not a valid email format.");
+      return false;
+    }
+    
+    const docSnapshot = await getDoc(doc(database, "Users", newUser.username));
+    if(docSnapshot.exists()){
+      setSignUpIndicator("Username already exists.");
       return false;
     }
     return true;
   }
+  async function signUpSuccess() {
+    await createUserWithEmailAndPassword(auth, newUser.email, newUser.password);
+
+    const userData = {
+      userName: newUser.username,
+      email: newUser.email,
+      heightestWPM: 0,
+      averageWPM: 0,
+      lessons: 0,
+      totalWords: 0,
+      totalErrors: 0,
+      totalTime: 0,
+    };
+    createNewUser("Users", userData);
+    setSignUpIndicator(`User ${newUser.username} Created!`);
+    setNewUser({ username: "", email: "", password: "", reEnterPassword: "" });
+  }
 
   async function submit(event) {
     event.preventDefault();
-    if(validSignUp()){
-      try{
-             await createUserWithEmailAndPassword(
-               auth,
-               newUser.email,
-               newUser.password
-             );
-      }catch(error){
+    if (await validSignUp()) {
+      try {
+        signUpSuccess();
+      } catch (error) {
         console.error(error);
       }
     }
@@ -73,7 +102,9 @@ export default function SignUpPopUp({ popUpState, setPopUp, toggleState }) {
           placeholder="Re-enter Password"
           type="password"
           value={newUser.reEnterPassword}
-          onChange={(e) => setNewUser({ ...newUser, reEnterPassword: e.target.value })}
+          onChange={(e) =>
+            setNewUser({ ...newUser, reEnterPassword: e.target.value })
+          }
         ></input>
         <br />
         <button type="submit" onClick={submit}>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,48 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from 'firebase/auth'
+import {getAuth, connectAuthEmulator} from 'firebase/auth'
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth/cordova";
+import { doc, setDoc, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCuijJTauVbY8LI4uCRd1kxk5JQ0UV8Z_w",
-  authDomain: "better-type.firebaseapp.com",
-  projectId: "better-type",
-  storageBucket: "better-type.appspot.com",
-  messagingSenderId: "850005982436",
-  appId: "1:850005982436:web:d9021d0f4dfaf6cfc5c0f1",
-  measurementId: "G-5ZZ60DD1GS"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-console.log(getAuth());
-console.log(getAuth().currentUser);
+export const googleProvider = new GoogleAuthProvider();
+export const database = getFirestore(app);
+
+//Dev Mode
+// if (window.location.hostname.includes("127.0.0.1")) {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099");
+  connectFirestoreEmulator(database, "127.0.0.1", 8080);
+  
+// }
+//Dev Mode End
+
+export async function createNewUser(collectionName, data){
+    try{
+      const docRef = await setDoc(doc(database, "Users", data.userName), data);
+      console.log('Document Created with: ', docRef.id);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+// console.log(getAuth());
+// console.log(getAuth().currentUser);
